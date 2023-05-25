55fbdf64282716709079a9cf8edc8ca8b5b852a9
jucruz <jucruz@exist.com>
2023-05-25 14:03:11 +0800

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Employee} from "./Employee";
+import {EmployeeService} from "./employee.service";
+import {HttpErrorResponse} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'employeemanagerangular';
+export class AppComponent implements OnInit{
+  title = 'Employee Manager';
+  public employees: Employee[]
+
+  constructor(private employeeService: EmployeeService) {}
+
+  ngOnInit(): void {
+    this.getEmployees();
+  }
+  public getEmployees(): void {
+    this.employeeService.getEmployees().subscribe(
+      (response) => {
+        this.employees = response;
+      },
+      (error : HttpErrorResponse) => {
+        alert(error.message);
+      }
+    )
+  }
+
+
 }
